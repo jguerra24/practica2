b5bd3c673eb228d66fb3060c49007db55d23d9b9
Jaime Guerra <61339190+jguerra24@users.noreply.github.com>
2022-03-23 21:13:03 -0700

    cambio en segundo println

--- prueva.java
+++ prueva.java
@@ -1,7 +1,7 @@
 class prueba{
     public static void main(String[] args){
         System.out.println("Esta es la practica 2 de jenkins, saludos.");
-        System.out.println("La conexión con GitHub se ha hecho");
+        System.out.println("La conexión con GitHub se ha confirmado");
         //comentario prueba
         //probando
     }
