fc3f816a54dfac644de3bb035ef346a5fec05341
Jaime Guerra <61339190+jguerra24@users.noreply.github.com>
2022-03-23 20:55:22 -0700

    se añadió una linea al programa

--- prueva.java
+++ prueva.java
@@ -1,5 +1,6 @@
 class prueba{
     public static void main(String[] args){
         System.out.println("Esta es la practica 2 de jenkins, saludos.");
+        System.out.println("La conexión con GitHub se ha hecho");
     }
 }
